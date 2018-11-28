3edf4e12f5f4d93068e3c748dddcc4c17135fd96
Elizabeth Messick <Elizabeth@MacBook-Pro-4.local>
2018-11-27 18:22:54 -0800

    add routing and pages components

---
+++ src/pages/Resources.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Resources = (props) => (
+    <div className="Resources">
+        This is the resources page
+    </div>
+);
+
+export default Resources;
---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Welcome = (props) => (
+    <div className="Welcome">
+        This is the welcome page
+    </div>
+);
+
+export default Welcome;
+
