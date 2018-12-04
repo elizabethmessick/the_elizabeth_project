c974ac1ffa3ddf9425a1f11731b80a8905475e7e
Elizabeth Messick <Elizabeth@MacBook-Pro-4.local>
2018-12-04 15:50:51 -0800

    fix resources page

--- src/pages/Resources/Resources.jsx
+++ src/pages/Resources/Resources.jsx
@@ -4,27 +4,31 @@ import './Resources.css';
 const Resources = (props) => (
     <div className="Resources">
         <h1>Victim Resources</h1>
-        <p><a href="http://www.cybercivilrights.org/faqs-usvictims/" target="_blank" rel="noopener noreferrer">FAQs for US NCP Victims</a> -Frequently asked questions and their answers for victims of NCP residing in the US.</p>
+        <div id="resource-list">
 
-        <p><a href="http://www.cybercivilrights.org/ccri-crisis-helpline/" target="_blank" rel="noopener noreferrer">CCRI National Helpline (844-878-2274)</a>– Free, confidential support available 24/7 for victims of nonconsensual pornography (NCP).</p>
+            <p><a href="http://www.cybercivilrights.org/faqs-usvictims/" target="_blank" rel="noopener noreferrer">FAQs for US NCP Victims</a> -Frequently asked questions and their answers for victims of NCP residing in the US.</p>
 
-        <p><a href="http://www.dmcadefender.com" target="_blank" rel="noopener noreferrer">DMCA Defender</a>– A thorough and efficient takedown service.  Be sure to mention that CCRI referred you.</p>
+            <p><a href="http://www.cybercivilrights.org/ccri-crisis-helpline/" target="_blank" rel="noopener noreferrer">CCRI National Helpline (844-878-2274)</a>– Free, confidential support available 24/7 for victims of nonconsensual pornography (NCP).</p>
 
-        <p><a href="https://iheartmob.org/" target="_blank" rel="noopener noreferrer">Heartmob</a>– A resource for victims of online harassment not involving intimate images: Report harassment, receive support, and help end online harassment.</p>
+            <p><a href="http://www.dmcadefender.com" target="_blank" rel="noopener noreferrer">DMCA Defender</a>– A thorough and efficient takedown service.  Be sure to mention that CCRI referred you.</p>
 
-        <p><a href="http://www.cybercivilrights.org/online-removal/" target="_blank" rel="noopener noreferrer">Online Removal Guide</a>– Instructions on how to report NCP on all of the major social media and tech platforms.</p>
+            <p><a href="https://iheartmob.org/" target="_blank" rel="noopener noreferrer">Heartmob</a>– A resource for victims of online harassment not involving intimate images: Report harassment, receive support, and help end online harassment.</p>
 
-        <p><a href="http://www.cybercivilrights.org/professionals-helping-victims/" target="_blank" rel="noopener noreferrer">Attorneys</a>– Lawyers that have volunteered to assist victims with their cases.</p>
+            <p><a href="http://www.cybercivilrights.org/online-removal/" target="_blank" rel="noopener noreferrer">Online Removal Guide</a>– Instructions on how to report NCP on all of the major social media and tech platforms.</p>
 
-        <p><a href="http://www.cybercivilrights.org/revenge-porn-laws/" target="_blank" rel="noopener noreferrer">“Revenge Porn” Laws</a>– Lists which states in the US have laws against NCP.</p>
+            <p><a href="http://www.cybercivilrights.org/professionals-helping-victims/" target="_blank" rel="noopener noreferrer">Attorneys</a>– Lawyers that have volunteered to assist victims with their cases.</p>
 
-        <p><a href="http://www.cybercivilrights.org/related-laws/" target="_blank" rel="noopener noreferrer">Related Laws</a>– US laws that may apply in an NCP case. Helpful when no “revenge porn” law exists in a victim’s state.</p>
+            <p><a href="http://www.cybercivilrights.org/revenge-porn-laws/" target="_blank" rel="noopener noreferrer">“Revenge Porn” Laws</a>– Lists which states in the US have laws against NCP.</p>
 
-        <p><a href="http://rcjlawgroup.com/2013/02/12/helpful-tips-for-victims-of-revenge-porn/" target="_blank" rel="noopener noreferrer">Helpful Tips for Victims of “Revenge Porn”</a>– A list of FAQs and responses for victims of NCP.</p>
+            <p><a href="http://www.cybercivilrights.org/related-laws/" target="_blank" rel="noopener noreferrer">Related Laws</a>– US laws that may apply in an NCP case. Helpful when no “revenge porn” law exists in a victim’s state.</p>
 
-        <p><a href="http://www.withoutmyconsent.org/" target="_blank" rel="noopener noreferrer">Without My Consent</a>– Non-profit seeking to combat online invasions of privacy.</p>
+            <p><a href="http://rcjlawgroup.com/2013/02/12/helpful-tips-for-victims-of-revenge-porn/" target="_blank" rel="noopener noreferrer">Helpful Tips for Victims of “Revenge Porn”</a>– A list of FAQs and responses for victims of NCP.</p>
 
-        <p><a href="http://www.cybercivilrights.org/intl-victim-resources/" target="_blank" rel="noopener noreferrer">International Victim Resources</a>– A list of resources available to victims residing outside the US.</p>    </div>
+            <p><a href="http://www.withoutmyconsent.org/" target="_blank" rel="noopener noreferrer">Without My Consent</a>– Non-profit seeking to combat online invasions of privacy.</p>
+
+            <p><a href="http://www.cybercivilrights.org/intl-victim-resources/" target="_blank" rel="noopener noreferrer">International Victim Resources</a>– A list of resources available to victims residing outside the US.</p>
+        </div>
+    </div>
 );
 
 export default Resources;
